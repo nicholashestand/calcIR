e8fef764e88b9589fe279fc370124aeb88453c4e
Nicholas Hestand <nicholasjhestand@gmail.com>
2017-09-28 17:47:52 -0500

    change header for use with GPU

--- calcIR.h
+++ calcIR.h
@@ -4,8 +4,13 @@
 #include <xdrfile/xdrfile.h>
 
 void get_eproj( rvec *x, float boxl, int natoms, int natom_mol, int nchrom, int nchrom_mol, int nmol, float *eproj);
+__global__
+void get_eproj_GPU( rvec *x, float boxl, int natoms, int natom_mol, int nchrom, int nchrom_mol, int nmol, float *eproj);
+__host__ __device__
 float minImage( float dx, float boxl );
+__host__ __device__
 float mag( float *dx );
+__host__ __device__
 float dot3( float *x, float *y );
 
 #endif
